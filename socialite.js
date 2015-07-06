37c736f0bdf5751272c11f5f91f1bc42bf1c016f
sanketgujarathi <sanketgujarathi@gmail.com>
2015-07-06 23:49:55 +0530

    Initial commit

---
+++ socialite.js
@@ -0,0 +1,19 @@
+(function($){
+
+    
+		$("#example").dateDropdowns({
+
+// Populate the widget with a default date.
+defaultDate: null,
+
+// The format of the date string provided to defaultDate
+defaultDateFormat: "yyyy-mm-dd",
+
+// Specify the order in which the dropdown fields should be rendered.
+displayFormat: "mdy",
+monthFormat: "short",
+wrapperClass:"dropdown"
+
+});
+
+})(jQuery);
